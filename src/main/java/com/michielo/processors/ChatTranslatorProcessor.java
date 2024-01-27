4a5c80e365b332347026d455c28fa3f0a51c17f9
Michielo <michiel.kamphuis@hetnet.nl>
2024-01-27 15:15:34 +0100

    Move translation API connection to separate class

--- src/main/java/com/michielo/processors/ChatTranslatorProcessor.java
+++ src/main/java/com/michielo/processors/ChatTranslatorProcessor.java
@@ -3,6 +3,7 @@ package com.michielo.processors;
 import com.michielo.Main;
 import com.michielo.player.PlayerData;
 import com.michielo.player.PlayerDataManager;
+import com.michielo.translation.Translator;
 import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
@@ -38,6 +39,7 @@ public class ChatTranslatorProcessor {
     private boolean allowFreeLanguageChoice;
     private boolean forceLanguage;
     private String forcedLanguage;
+    private Translator translator;
 
     public ChatTranslatorProcessor() {
         FileConfiguration file = Main.getInstance().getConfig();
@@ -51,6 +53,8 @@ public class ChatTranslatorProcessor {
             Bukkit.getLogger().severe("As this contradicts each other, we will allow free language choice and not force the language!");
             this.forceLanguage = false;
         }
+
+        this.translator = new Translator();
     }
 
     /*
@@ -86,90 +90,12 @@ public class ChatTranslatorProcessor {
         // Loop through each language and perform some operation
         for (String language : playersByLanguage.keySet()) {
             List<PlayerData> playersForLanguage = playersByLanguage.get(language);
-            String translation = getTranslation(original,
+            String translation = this.translator.getTranslation(original,
                     PlayerDataManager.getInstance().getPlayerData(sender).getLanguage(), language);
             for (PlayerData player : playersForLanguage) {
-                player.getPlayer().sendMessage(sender.getDisplayName() + " >> " + extractTranslation(translation));
+                player.getPlayer().sendMessage(sender.getDisplayName() + " >> " + this.translator.extractTranslation(translation));
             }
         }
-    }
-
-    /*
-        Util
-     */
-
-    private String getTranslation(String text, String src, String target) {
-        try {
-            String apiUrl = "http://159.69.60.114:8000/translate"; // Update with your actual IP and port
-
-            // Create a URL object with the API endpoint
-            URL url = new URL(apiUrl);
-
-            // Open a connection to the URL
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-
-            // Set the request method to POST
-            connection.setRequestMethod("POST");
-
-            // Enable input/output streams
-            connection.setDoOutput(true);
-            connection.setDoInput(true);
-
-            // Set the content type to JSON
-            connection.setRequestProperty("Content-Type", "application/json");
-
-            // Create JSON payload
-            String jsonInputString = "{\"text\":\"" + text + "\",\"source_lang\":\"" + src
-                    + "\",\"target_lang\":\"" + target + "\"}";
-
-            // Write the JSON payload to the connection's output stream
-            try (OutputStream os = connection.getOutputStream()) {
-                byte[] input = jsonInputString.getBytes("utf-8");
-                os.write(input, 0, input.length);
-            }
-
-            // Get the response from the server
-            StringBuilder response = new StringBuilder();
-            try (BufferedReader br = new BufferedReader(
-                    new InputStreamReader(connection.getInputStream(), "utf-8"))) {
-                String responseLine;
-                while ((responseLine = br.readLine()) != null) {
-                    response.append(responseLine.trim());
-                }
-            }
-
-            // Close the connection
-            connection.disconnect();
-
-            // return response
-            return response.toString();
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
-
-    private static String extractTranslation(String jsonString) {
-        // Find the index of the "translation" key
-        int startIndex = jsonString.indexOf("\"translation\":");
-        if (startIndex == -1) {
-            // "translation" key not found, return original string
-            return jsonString;
-        }
-
-        // Find the start of the value associated with the "translation" key
-        startIndex = jsonString.indexOf("\"", startIndex + 13) + 1;
-
-        // Find the end of the value associated with the "translation" key
-        int endIndex = jsonString.indexOf("\"", startIndex);
-
-        if (startIndex == -1 || endIndex == -1) {
-            // Issue with JSON structure, return original string
-            return jsonString;
-        }
-
-        // Extract the value associated with the "translation" key
-        return jsonString.substring(startIndex, endIndex);
     }
 
 }
